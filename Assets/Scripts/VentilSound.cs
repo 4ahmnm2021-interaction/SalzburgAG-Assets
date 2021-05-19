093aca39e1c8ee138e6c541aa7975ff5501a2b1f
GoetteVictoria <victoria.goette01@htl-salzburg.ac.at>
2021-05-19 12:44:13 +0200

    edit script

---
+++ Assets/Scripts/VentilSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VentilSound : MonoBehaviour
+{
+
+    public AudioClip Ventil;
+    public AudioSource audios;
+
+
+
+    public void VentilSounds()
+    {
+        audios.PlayOneShot(Ventil);
+    }
+}
